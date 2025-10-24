5a20e64dcfe50e71243931de9d41c3a5b072f1fd
Hurmex <www.seznam666@seznam.cz>
2025-10-24 12:11:30 +0200

    revert filip branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import './App.css'
-import Page1 from './pages'
-import DanovaVetev from './pages/DanovaVetev/DanovaVetev'
 
 function App() {
 
   return (
     <>
-      <Page1/>
-      <DanovaVetev/>
+
     </>
   )
 }
